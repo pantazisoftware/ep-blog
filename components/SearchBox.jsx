8747c99a4a4428e0d0e4bbfeeef75fd47c8e8e36
pantazisoftware <robert.pantazi02@gmail.com>
2022-12-29 00:55:40 +0200

    Searchbox and MDX

--- components/SearchBox.jsx
+++ components/SearchBox.jsx
@@ -1,6 +1,6 @@
 const SearchBox = ({placeholder}) => {
     return (
-        <div className="bg-white/30 p-6">
+        <div className="bg-red-500/30 p-6">
             <input type="text" placeholder={placeholder} className="p-2 text-lg font-bold rounded-lg outline-none" />
         </div>
     )
